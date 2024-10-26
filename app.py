76ec30914598df4a84d835ab67da376e32945b9f
Mukul Kumar <101646861+MukulKumar56@users.noreply.github.com>
2024-10-26 20:22:06 +0530

    app.py

--- app.py
+++ app.py
@@ -56,4 +56,4 @@ def home():
                            title=title, pdf_link=pdf_link, message=message)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
